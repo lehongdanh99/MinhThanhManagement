3835590fb935ba3218d882a1c1236750c4716169
nguyenanh <17110094@student.hcmute.edu.vn>
2023-03-07 12:45:35 +0700

    add view

---
+++ MinhThanhManagement/View/HistoryView.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MinhThanhManagement.View
+{
+    /// <summary>
+    /// Interaction logic for HistoryView.xaml
+    /// </summary>
+    public partial class HistoryView : Window
+    {
+        private static HistoryView _instance;
+        public static HistoryView GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new HistoryView();
+            }
+            return _instance;
+        }
+        public HistoryView()
+        {
+            InitializeComponent();
+        }
+
+        public HistoryView historyView = new HistoryView();
+
+
+        private void closeApp(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
