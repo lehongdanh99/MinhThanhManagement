ae795a45835d8a17b7c78c1580f3a58b98699a44
nguyenanh <17110094@student.hcmute.edu.vn>
2023-03-21 09:40:58 +0700

    update bill print

--- MinhThanhManagement/View/BillView.xaml.cs
+++ MinhThanhManagement/View/BillView.xaml.cs
@@ -65,13 +65,22 @@ namespace MinhThanhManagement.View
                     CancelButton.Visibility = Visibility.Collapsed;
                     dataGridTempDelete.Visibility = Visibility.Collapsed;
                     PrintButton.Visibility = Visibility.Collapsed;
-                    this.Hide();
+                    
                     PrintDialog printDialog = new PrintDialog();
                     if (printDialog.ShowDialog() == true)
                     {
-                        printDialog.PrintVisual(billPage, "Invoice");
-                        MessageBox.Show("Đã xuất file PDF");
                         
+                        printDialog.PrintVisual(billPage, "Invoice");
+
+                        MessageBox.Show("Đã xuất file PDF");
+                        this.Hide();
+                        AddGroupStackpanel.Visibility = Visibility.Visible;
+                        CancelButton.Visibility = Visibility.Visible;
+                        dataGridTempDelete.Visibility = Visibility.Visible;
+                        PrintButton.Visibility = Visibility.Visible;
+                    }
+                    else
+                    {
                         AddGroupStackpanel.Visibility = Visibility.Visible;
                         CancelButton.Visibility = Visibility.Visible;
                         dataGridTempDelete.Visibility = Visibility.Visible;
